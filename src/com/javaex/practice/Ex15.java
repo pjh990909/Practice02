e9ceb994470c03489c2a4699ec535cbff361ec61
pjh990909 <pjh990909@naver.com>
2023-12-27 21:21:04 +0900

    15번 해결

--- src/com/javaex/practice/Ex15.java
+++ src/com/javaex/practice/Ex15.java
@@ -9,27 +9,35 @@ public class Ex15 {
 		Scanner sc = new Scanner(System.in); 
 		
 		System.out.println("출력되는 내용을 입력하세요");
-		System.out.print("기호: ");
+		System.out.print("기호:");
 		String sign = sc.nextLine();
 		
 		System.out.print("숫자1: ");
 		double num01 = sc.nextDouble();
-		
+		System.out.print("숫자2: ");
 		double num02 = sc.nextDouble();
 		
-
-		if(num01/num02==0&&num02 == 0) {
-			System.out.println("계산할 수 없습니다.");
-		}else if(sign == "+"){
-			System.out.println("결과는: "+ num01+num02 );
-		}else if(sign == "-") {
-			System.out.println("결과는: "+ (num01-num02) );
-		}else if(sign == "/") {
-			System.out.println("결과는: " + num01/num02);
-		}else {
-			System.out.println("결과는: "+ num01*num02);
-		}
-			
+		switch (sign) {
+        case "+":
+            System.out.println("결과는: " + (num01 + num02));
+            break;
+        case "-":
+            System.out.println("결과는: " + (num01 - num02));
+            break;
+        case "/":
+            if (num02 == 0) {
+                System.out.println("계산할 수 없습니다.");
+            } else {
+                System.out.println("결과는: " + (num01 / num02));
+            }
+            break;
+        case "*":
+            System.out.println("결과는: " + (num01*num02));
+            break;
+        default:
+            System.out.println("올바른 기호를 입력하세요.");
+    }
+		
 		
 		
 		sc.close();
